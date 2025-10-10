d68c374c7f7b067b486bae1782defed181cba6bb
LE BAR <letruong8017@gmail.com>
2025-10-10 09:28:50 +0700

    Configure MVC and load some necessary packages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const app = express();
-const port = 8888;
+require("dotenv").config();
 
+const app = express();
+const port = process.env.PORT || 8081;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World GIOFCHAR!");
 });
 
 app.listen(port, () => {
